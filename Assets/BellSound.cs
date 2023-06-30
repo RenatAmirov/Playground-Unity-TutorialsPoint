b39807fac7a274b087d9e33a7104a1414a8b83a1
rinux <rinux@DESKTOP-7Q33P48>
2023-06-30 08:48:39 +0400

    Playground until p56.

--- Assets/BellSound.cs
+++ Assets/BellSound.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BellSound : MonoBehaviour
 {
+    AudioSource mySource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mySource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked!");
+        mySource.Play();
     }
 }
